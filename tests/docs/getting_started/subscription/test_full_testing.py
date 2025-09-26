60d47821dfa24d5f42dade8ebc2ca6640a9d4c2a
lubaskin <lubaskincorporation@gmail.com>
2025-09-26 19:07:27 +0300

    tests

---
+++ tests/docs/getting_started/subscription/test_full_testing.py
@@ -0,0 +1,69 @@
+import pytest
+
+from tests.marks import (
+    require_aiokafka,
+    require_aiopika,
+    require_confluent,
+    require_nats,
+    require_redis,
+)
+
+
+@pytest.mark.kafka()
+@pytest.mark.connected()
+@pytest.mark.asyncio()
+@require_aiokafka
+async def test_handle_kafka() -> None:
+    from docs.docs_src.getting_started.subscription.kafka.full_testing import (
+        test_handle as test_handle_k,
+    )
+
+    await test_handle_k()
+
+
+@pytest.mark.connected()
+@pytest.mark.confluent()
+@pytest.mark.asyncio()
+@require_confluent
+async def test_handle_confluent() -> None:
+    from docs.docs_src.getting_started.subscription.confluent.full_testing import (
+        test_handle as test_handle_confluent,
+    )
+
+    await test_handle_confluent()
+
+
+@pytest.mark.connected()
+@pytest.mark.asyncio()
+@pytest.mark.rabbit()
+@require_aiopika
+async def test_handle_rabbit() -> None:
+    from docs.docs_src.getting_started.subscription.rabbit.full_testing import (
+        test_handle as test_handle_r,
+    )
+
+    await test_handle_r()
+
+
+@pytest.mark.connected()
+@pytest.mark.asyncio()
+@pytest.mark.nats()
+@require_nats
+async def test_handle_nats() -> None:
+    from docs.docs_src.getting_started.subscription.nats.full_testing import (
+        test_handle as test_handle_n,
+    )
+
+    await test_handle_n()
+
+
+@pytest.mark.connected()
+@pytest.mark.asyncio()
+@pytest.mark.redis()
+@require_redis
+async def test_handle_redis() -> None:
+    from docs.docs_src.getting_started.subscription.redis.full_testing import (
+        test_handle as test_handle_rd,
+    )
+
+    await test_handle_rd()
